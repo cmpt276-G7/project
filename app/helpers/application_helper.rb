adae0aa88122efebbc3809fe7919af0d0e9e9921
J <djontung@yahoo.com>
2015-10-19 17:55:16 -0700

    initial layout (template) and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Group 7 Project Name"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
