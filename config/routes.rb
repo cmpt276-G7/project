35037130162d19b80edfdf9e54a236a76ad9a832
J <djontung@yahoo.com>
2015-10-19 23:34:31 -0700

    log in log out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,13 +14,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-   get signup_path
-   assert_difference 'User.count', 1 do
-     post_via_redirect users_path, user: { name:  "Example User",
-                                           email: "user@example.com",
-                                           password:              "password",
-                                           password_confirmation: "password" }
-   end
-   assert_template 'users/show'
- end
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
