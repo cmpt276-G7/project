7a32a535a6a1b7b568fe7e9b8e105765d1e707cd
J <djontung@yahoo.com>
2015-11-21 03:33:31 -0800

    little bits

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationsController < ApplicationController
   def index
     @user = current_user
 #    @authentication = Authentication.find_by(user_id: @user.id)
-    @authentications = Authentication.find_by(user_id: @user.id)
+    @authentications = Authentication.where(user_id: @user.id)
 
     if fb_authenticated?(@user)
       #fbauth = @authentications.find_by(provider: "facebook")
@@ -32,7 +32,7 @@ class AuthenticationsController < ApplicationController
     end
 
     #need to take all authentications later
-    @authentications = Authentication.where(user_id: @user.id).take
+    @authentications = Authentication.where(user_id: @user.id)
 
     #filling facebook data
     #if auth['provider'] == 'facebook'
