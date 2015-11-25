e4167cf759ebdce1bd9f3167354bcfa8d1d8c9fb
period23dijason <jasonw1230@hotmail.com>
2015-11-25 01:10:37 -0800

    carousel on home with logos, pic/username header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
+<<<<<<< Updated upstream
+=======
+  before_action :admin_user, only: :destroy
+
+  def index
+    @user = current_user
+    @users = User.paginate(page: params[:page])
+  end
+>>>>>>> Stashed changes
 
   def show
     @user = User.find(params[:id])
