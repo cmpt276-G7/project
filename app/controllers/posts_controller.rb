56eeb1cda2f78942d557120ce613fb3111a07f05
J <djontung@yahoo.com>
2015-11-27 01:41:51 -0800

    changed schema a lot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,30 @@ class PostsController < ApplicationController
 
   def index
     @user = current_user
+    @user.posts.destroy_all
 
+
+    #get facebook posts
     if fb_authenticated?(@user)
       auth = @user.authentications.find_by(provider: 'facebook')
-      @posts = Post.FBget(auth['token'])
+      posts = Post.FBget(auth['token'])
+
+      posts.each do |post|
+        @user.posts.create(author: post['from']['name'], message: post['message'],
+        provider: "facebook", picture: post['from']['picture']['data']['url'].to_s,
+        link: post['link'], timeposted: post['created_time'].to_time)
+      end
+
     end
 
-    @posts2 = Post.Twitterget("asdf")
+    #get twitter posts
+    if twitter_authenticated?(@user)
+      twitterauth = @user.authentications.find_by(provider: 'twitter')
+      posts2 = Post.Twitterget(twitterauth.token, twitterauth.secret)
+    end
+
+    #add to @posts instance var
+    @posts = @user.posts.all
 
   end
 
