62f159af0bb1ae5f40f1e87dd07be4522cf93baf
J <djontung@yahoo.com>
2015-12-01 01:24:48 -0800

    some css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,23 @@
 class StaticPagesController < ApplicationController
+	include SessionsHelper
+
   def home
+		@user = current_user
   end
 
   def help
+  		@user = current_user
   end
 
   def about
+  		@user = current_user
   end
 
   def contact
+  		@user = current_user
   end
+
+
 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Group 7 Project Name"
+    base_title = "SMCrate"
     if page_title.empty?
       base_title
     else
